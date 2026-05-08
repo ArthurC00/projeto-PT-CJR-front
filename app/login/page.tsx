cde38ad5e24e8b7ae287bf240e5007347718afd6
Ana Luisa <camara.analui@gmail.com>
2026-05-07 21:27:45 -0300

    feat: alinhei e ajustei o tamanho das imagens e dos campos de entrada da tela de login (últimos ajustes)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,39 +2,39 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 export default function Login() { return (
-    <main className="bg-[#F6F3E4] flex items-center">
-        <div className="">
-            <Image
+    <main className="bg-[#F6F3E4] flex flex-row items-center justify-center justify-items-center h-screen w-screen">
+        <div className="h-screen w-[50vw] justify-items-center">
+            <Image className="w-auto h-1/4 self-center"
             src="/images/logo_stock.io.png"
             width={421}
             height={222}
             alt="Logo Stock.io"
             />
-            <Image
+            <Image className="h-3/4 w-auto self-end"
             src="/images/stockles_caixa.png"
             width={514}
             height={802}
             alt="Mascote Stockles segurando uma caixa."/>
         </div>
-        <div className="bg-[#171918] p-12 self-end rounded-4xl text-center font-sans ">
+        <div className="h-[85vh] w-[50vw] bg-[#171918] mr-16 pt-12 rounded-t-4xl text-center justify-self-start justify-center place-items-center font-sans self-end">
             <h1 className="font-black text-4xl m-6 tracking-wide">BEM VINDO DE VOLTA!</h1>
-            <form className="flex flex-col">
+            <form className="flex flex-col w-5/6">
                 <input required
                 type="email"
                 name="email"
                 placeholder="Email"
                 aria-label="E-mail"
                 // inputs: w-[504px] h-[42px]
-                className="bg-[#FFFFFF] text-black rounded-full m-2 h-10 pl-8"
+                className="bg-[#FFFFFF] text-black rounded-full my-3 h-10 w-full pl-8"
                 />
                 <input required
                 type="password"
                 name="senha"
                 placeholder="Senha"
                 aria-label="Senha"
-                className="bg-[#FFFFFF] text-black rounded-full m-2 h-10 pl-8"
+                className="bg-[#FFFFFF] text-black rounded-full my-3 h-10 w-full pl-8"
                 />
-                <p className="m-4 font-thin text-sm"><Link href="/recuperar-conta" className="underline">Esqueceu sua senha?</Link></p>
+                <p className="m-4 font-thin text-md"><Link href="/recuperar-conta" className="underline">Esqueceu sua senha?</Link></p>
                 <button type="submit" className="bg-[#6A38F3] font-bold rounded-full m-2 h-11">ENTRAR</button>
                 <p className="text-start m-4 text-lg font-extralight">Não possui uma conta? <Link href="/cadastro" className="text-[#6A38F3] font-medium">Cadastre-se</Link></p>
             </form>
