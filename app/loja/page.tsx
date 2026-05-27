04867703119734af800357ad799d25e78ec7599d
Juju Luz <julialuz@MacBook-Air-de-Julia.local>
2026-05-26 21:56:32 -0300

    feat:implementando imagem na tela

--- app/loja/page.tsx
+++ app/loja/page.tsx
@@ -1,7 +1,11 @@
 "use client"
 
 import Navbar from "@/components/navbar";
+import Image from "next/image";
 import {useState} from "react";
+import garotas from "../../public/imagem_loja.svg"
+import escuro from "../../public/telaloja_degrade.svg"
+import nome from "../../public/rareBeauty_loja.png"
 
 
 
@@ -21,6 +25,22 @@ export default function Tela_loja(){
     return(
         <div className = "flex flex-col min-h-screen bg-black">
             <Navbar/> 
+            <div className="relative">
+            <Image src={escuro} alt = { "escuro"} 
+            height={10}
+            width={10000}
+            className="absolute z-10 top-0 left-0"
+            ></Image>
+
+            <Image src={garotas} alt={"Garotas"}
+            height={10}
+            width={10000}></Image>
+            className= "relative z-0"
+            </div>
+
+            <div className = "w-full min-h-screen items-center justify-center ">
+                <Image src={nome} alt={"nome"}></Image>
+            </div>
         </div>
     )
 }
