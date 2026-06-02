353c35f306387b9d63af18d4b224bfa6b105052f
Juju Luz <julialuz@MacBook-Air-de-Julia.local>
2026-06-02 14:28:34 -0300

    feat: background da tela de comentários de avaliação

---
+++ app/com_aval/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import Navbar from "@/components/navbar";
+import { useState, useEffect } from "react";
+
+
+export default function com_aval(){
+    
+
+    return(
+        <div className=" min-h-scree overflow-x-auto " >
+            <Navbar />
+            <div className="bg-black py-30"></div>
+
+        </div>
+    )
+}
