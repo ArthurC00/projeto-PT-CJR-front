49756934a39bbe50cfa898045de85991374cecae
Arthur <aoscunhaa@gmail.com>
2026-06-08 10:53:27 -0300

    feat: integracao da tela de feed

--- app/services/api.ts
+++ app/services/api.ts
@@ -17,6 +17,43 @@ interface Cadastro {
   foto_perfil_url: string;
 }
 
+interface produtos {
+  id: number;
+  nome: string;
+  preço: number;
+  categoria: categorias
+}
+
+interface categorias {
+  id: number;
+  nome: string;
+  slug: string;
+}
+
+export interface Produto {
+  id: number;
+  nome: string;
+  preco: number;
+  categoria_id: number;
+  categoria: {
+  select: {
+    id: true,
+    nome: true,
+      }
+    },
+  imagens: {
+    url_imagem: string;
+    ordem: number;
+  }[];
+}
+
+export interface Categoria {
+  id: number;
+  nome: string;
+  slug: string;
+}
+
+
 export const postLogin = async (body: Login) => {
   try {
     const response = await api.post(`/login`, body);
@@ -43,3 +80,30 @@ export const getOneProduct = async (id: string) => {
     throw new Error(error.message);
   }
 };
+
+export const getProdutos = async () => {
+  try {
+    const response = await api.get('/produto');
+    return response.data;
+  } catch ( e: any ) {
+    throw new Error(e.message);
+  }
+}
+
+export const getCategorias = async () => {
+  try {
+    const response = await api.get('/categorias');
+    return response.data;
+  } catch ( e: any ) {
+    throw new Error(e.message);
+  }
+}
+
+export const getCategoriasRaiz = async () => {
+  try {
+    const response = await api.get('/categorias/raiz');
+    return response.data;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+}
